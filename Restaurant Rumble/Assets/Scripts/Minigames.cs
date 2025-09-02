0db1310efaebc566b3417af268b4dcd96c74959e
Maya <hochm8245@g.comalisd.org>
2025-09-02 10:22:24 -0500

    minigame framework

---
+++ Restaurant Rumble/Assets/Scripts/Minigames.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Minigames : MonoBehaviour
+{
+    // game 1 is timing 
+    //game 2 is matching 
+    //game 3 is ordering 
+    // game 4 is balancing 
+    void Update()
+    {
+        
+    }
+
+    void interact()
+    {
+
+    }
+}
