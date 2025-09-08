3a4f8b02626b069369a36a9faa6c6a85f3681b46
Maya <hochm8245@g.comalisd.org>
2025-09-08 10:08:31 -0500

    MINIGAME WITH STUFF OMG I DID IT

--- Restaurant Rumble/Assets/Scripts/Minigame behaviours/TimingMinigame.cs
+++ Restaurant Rumble/Assets/Scripts/Minigame behaviours/TimingMinigame.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class TimingMinigame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public int pointRequirement;
+    public int currentPoints = 0;
+    private bool ActiveThingy;
+    private GameObject Dave;
     void Update()
     {
-        
+        if (ActiveThingy&&Input.GetKeyDown(KeyCode.Space)) {
+            currentPoints++;
+            Dave.GetComponent<Image>().color = Color.green;
+            Dave.GetComponent<Collider2D>().enabled = false;
+        }
+    
+
+        if (currentPoints == pointRequirement) Debug.Log("You DID IT :D"); //WHAT DO YOU WANT FORM MEEE YOU IDIOTIC CABBAGE YOU GRAOUND BEEF FOOL YOUJ UGLY LINE OF TEXT WITH NO JOB YOYU UGLY UNNATRACTIVE DBIHYFGSTYIUDHFCTUGYHFDRETFUYGIHDCFYTUGIHFCWDRTYG I HATE YOU 
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("entered timing point");
+        //If button press seperated into 2 
+        ActiveThingy = true;
+        Dave = other.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        ActiveThingy=false;
+        Dave = null;
     }
 }
