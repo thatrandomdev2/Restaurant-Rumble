c6bf0bdd619dabdf8a5bc27333ef694403c9b61f
thatrandomdev2 <diazm9707@g.comalisd.org>
2025-09-10 09:36:06 -0500

    Camera Updated

--- Restaurant Rumble/Assets/Scripts/CameraScript.cs
+++ Restaurant Rumble/Assets/Scripts/CameraScript.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     GameObject player;
+    Vector3 targetPos;
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        targetPos = transform.position;
     }
     void Update()
     {
-        
+        //distanceToPlayer = transform.position - player.transform.position;
+        targetPos = Vector3.Lerp(targetPos, player.transform.position, 0.01f);
+        transform.position = targetPos + new Vector3(0,10,-11);
     }
 }
