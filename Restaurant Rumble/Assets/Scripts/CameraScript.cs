28c16d4a8ecf0ce9067095ca948ce4248038063c
thatrandomdev2 <diazm9707@g.comalisd.org>
2025-09-10 09:26:18 -0500

    Player Prefab

---
+++ Restaurant Rumble/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    void Update()
+    {
+        
+    }
+}
