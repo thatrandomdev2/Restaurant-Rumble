f40ff35096270d5387699459e8a8e57b2f2deec9
Maya <hochm8245@g.comalisd.org>
2025-09-10 09:20:43 -0500

    Minigame Trigger update

--- Restaurant Rumble/Assets/Scripts/MinigameTrigger.cs
+++ Restaurant Rumble/Assets/Scripts/MinigameTrigger.cs
@@ -9,20 +9,26 @@ public class MinigameTrigger : MonoBehaviour
     //game 2 is matching 
     //game 3 is ordering 
     // game 4 is balancing
+    private bool InInteractArea;
     [SerializeField] Collider InteractArea;
     [SerializeField] Canvas MinigameScene; //i have never wanted to tell a piece of text to end its own life before but that might change RIGHT HERE APPARENTLY
     void Update()
     {
-        
+       if (InInteractArea&&Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("you interacted with the thingy");
+        } 
     }
 
     void OnTriggerEnter(Collider other) //minigame trigger & appear
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("you interacted with the thingy");
-        }
+
+        InInteractArea = true;
         
-        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+      InInteractArea=false;  
     }
 }
