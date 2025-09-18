c5e52f52ecb1ec81d3469ae668a78f42376536ca
thatrandomdev2 <diazm9707@g.comalisd.org>
2025-09-18 10:09:54 -0500

    Inventory Stuff

---
+++ Restaurant Rumble/Assets/InventoryManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    GameObject player;
+    public PlayerScript playerScript;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
